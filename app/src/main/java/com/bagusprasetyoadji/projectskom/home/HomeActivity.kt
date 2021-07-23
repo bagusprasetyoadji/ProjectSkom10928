c23da169ae61a49fce51447b7dd5ebb35eade935
Bagus Prasetyo Adji <bagusprasetyaadji@gmail.com>
2021-07-23 15:11:49 +0700

    Project Skom Batch 1

--- app/src/main/java/com/bagusprasetyoadji/projectskom/home/HomeActivity.kt
+++ app/src/main/java/com/bagusprasetyoadji/projectskom/home/HomeActivity.kt
@@ -1,8 +1,14 @@
 package com.bagusprasetyoadji.projectskom.home
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.bagusprasetyoadji.projectskom.R
 import com.bagusprasetyoadji.projectskom.databinding.ActivityHomeBinding
 import com.bagusprasetyoadji.projectskom.login.LoginActivity
@@ -20,12 +26,31 @@ class HomeActivity : AppCompatActivity() {
 
         auth = FirebaseAuth.getInstance()
 
-        binding.btnLogout.setOnClickListener {
-            auth.signOut()
-            Intent(this@HomeActivity,LoginActivity::class.java).also { intent ->
-                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                startActivity(intent)
+        val navController = findNavController(R.id.nav_host_fragment)
+        val appBarConfiguration = AppBarConfiguration.Builder(
+            R.id.nav_home, R.id.nav_profile
+        ).build()
+        setupActionBarWithNavController(navController,appBarConfiguration)
+
+        binding.navBottom.setupWithNavController(navController)
+
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.option_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.logout -> {
+                auth.signOut()
+                Intent(this@HomeActivity,LoginActivity::class.java).also { intent ->
+                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    startActivity(intent)
+                }
+                return true
             }
+            else -> return true
         }
     }
 }
